28ead2ae3a7b1a0146cbac80a1ffca4de19d2525
alex2frisky <alex.bazilescu@gmail.com>
2026-03-01 08:25:30 +0200

    changed var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@ variable "docker_image" {
 }
 
 variable "public_key_path" {
-  description = "Path to your SSH public key (e.g., ~/.ssh/id_rsa.pub)"
+  description = "Path to your SSH public key (e.g., ~/.ssh/id_ed25519.pub)"
   type        = string
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_ed25519.pub"
 }
